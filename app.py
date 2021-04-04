f5e3d3965c8c2e4d57e3154784153569ea4b9b59
Brandon M. Flittner <bflittner@bnet.local>
2021-04-04 13:02:24 -0400

    push

--- app.py
+++ app.py
@@ -135,7 +135,7 @@ def initk8s():
     @after_this_request
     def run_initk8s():
         installers_k8s.init_k8s()
-    return jsonify({'Cluster deployed.'})
+    return jsonify({'Cluster initilizing.'})
 
 def healthCheck():
     con = open_connection()
