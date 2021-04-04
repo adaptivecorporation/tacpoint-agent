370594e6e0133358dcfb7d3d523c124d85569620
Brandon M. Flittner <bflittner@bnet.local>
2021-04-04 16:45:00 -0400

    test

--- installers_k8s.py
+++ installers_k8s.py
@@ -22,17 +22,17 @@ def init_k8s():
 
     subprocess.call('DEBIAN_FRONTEND=noninteractive apt install -y apt-transport-https', shell=True)
     subprocess.call('apt install -y ca-certificates curl software-properties-common', shell=True)
+    subprocess.call('systemctl restart docker', shell=True)
     dockerconf1_inp = {
-                        "exec-opts": ["native.cgroupdriver=systemd"],
-                        "log-driver": "json-file",
-                        "log-opts": {
-                            "max-size": "100m"
-                        },
-                        "storage-driver": "overlay2"
-                        }
+                    "exec-opts": ["native.cgroupdriver=systemd"],
+                    "log-driver": "json-file",
+                    "log-opts": {
+                        "max-size": "100m"
+                    },
+                    "storage-driver": "overlay2"
+                    }
     dockerconf = open("/etc/docker/daemon.json")
     dockerconf.write(json.dumps(dockerconf1_inp))
-    subprocess.call('systemctl restart docker', shell=True)
     subprocess.call('ufw disable', shell=True)
     subprocess.call('sudo curl -fsSLo /usr/share/keyrings/kubernetes-archive-keyring.gpg https://packages.cloud.google.com/apt/doc/apt-key.gpg', shell=True)
     subprocess.call('echo "deb [signed-by=/usr/share/keyrings/kubernetes-archive-keyring.gpg] https://apt.kubernetes.io/ kubernetes-xenial main" | sudo tee /etc/apt/sources.list.d/kubernetes.list', shell=True)
