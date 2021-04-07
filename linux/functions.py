ce9ae96d4de52e4d23554f9c90e12b50759dd554
Brandon M. Flittner <bflittner@bnet.local>
2021-04-06 20:10:02 -0400

    test

--- linux/functions.py
+++ linux/functions.py
@@ -3,6 +3,7 @@ import os
 
  
 def k8s_apply_conf():
+    subprocess.call('pwd')
     subprocess.call('kubectl apply -f ./uploads/k8s.conf', shell=True)
     subprocess.call('rm -rf k8s.conf', shell=True)
     return
