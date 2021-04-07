3852607f105c90ecdabba36b8ab4cbc450b33953
Brandon M. Flittner <bflittner@bnet.local>
2021-04-06 20:02:27 -0400

    conf remove

--- linux/functions.py
+++ linux/functions.py
@@ -3,7 +3,6 @@ import os
 
  
 def k8s_apply_conf():
-    subprocess.call('cd /root/', shell=True)
-    subprocess.call('kubectl apply -f k8s.conf', shell=True)
+    subprocess.call('kubectl apply -f /root/k8s.conf', shell=True)
     subprocess.call('rm -rf k8s.conf', shell=True)
     return
