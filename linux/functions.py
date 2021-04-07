8ba2f561185750fcd97e3e6429642eee6a7a7a2a
Brandon M. Flittner <bflittner@bnet.local>
2021-04-06 20:09:09 -0400

    test

--- linux/functions.py
+++ linux/functions.py
@@ -3,6 +3,6 @@ import os
 
  
 def k8s_apply_conf():
-    subprocess.call('kubectl apply -f /root/k8s.conf', shell=True)
+    subprocess.call('kubectl apply -f ./uploads/k8s.conf', shell=True)
     subprocess.call('rm -rf k8s.conf', shell=True)
     return
