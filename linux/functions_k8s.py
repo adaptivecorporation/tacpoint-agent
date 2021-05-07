4ab66166ddd0e9fcc515fce31925ab8c66f775c9
Brandon M. Flittner <bflittner@bnet.local>
2021-05-07 05:12:50 -0400

    fix

--- linux/functions_k8s.py
+++ linux/functions_k8s.py
@@ -7,6 +7,6 @@ def get_pods():
     if path.exists ():
         os.remove('./logs/k8s_pods.log')
 
-    f = open("./logs/k8s_pods.log", "w")
+    f = open("./logs/k8s_pods.log", "wr")
     subprocess.call('kubectl get pods --all-namespaces', shell=True, stdout=f)
     return f.text
