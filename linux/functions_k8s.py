36376692ffb5bf12e3d1a2d59a8a7e44c0dfd70e
Brandon M. Flittner <bflittner@bnet.local>
2021-05-07 05:11:41 -0400

    changes

--- linux/functions_k8s.py
+++ linux/functions_k8s.py
@@ -1,12 +1,11 @@
 import subprocess
 import os
+import pathlib
 
 def get_pods():
-    try:
+    path = pathlib.Path("./logs/k8s_pods.log")
+    if path.exists ():
         os.remove('./logs/k8s_pods.log')
-        
-    except Exception as error:
-        pass
 
     f = open("./logs/k8s_pods.log", "w")
     subprocess.call('kubectl get pods --all-namespaces', shell=True, stdout=f)
